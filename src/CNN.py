3224435061fa20fe0a161d96eb730d109864b778
oehamilton <oehamilton@hotmail.com>
2025-06-10 13:20:05 -0500

    Different Filter Values to observe the differences in the image changes

--- src/CNN.py
+++ src/CNN.py
@@ -25,11 +25,12 @@ print("Size of the image: ", size_x, size_y)
 # This filter detects edges nicely
 # It creates a filter that only passes through sharp edges and straight lines. 
 # Experiment with different values for fun effects.
+
 #filter = [ [0, 1, 0], [1, -4, 1], [0, 1, 0]] 
-# A couple more filters to try for fun!
-filter = [ [-1, -2, -1], [0, 0, 0], [1, 2, 1]]
-#filter = [ [-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
- # If all the digits in the filter don't add up to 0 or 1, you 
+#filter = [ [-1, -2, -1], [0, 0, 0], [1, 2, 1]]
+filter = [ [-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
+
+# If all the digits in the filter don't add up to 0 or 1, you 
 # should probably do a weight to get it to do so
 # so, for example, if your weights are 1,1,1 1,2,1 1,1,1
 # They add up to 10, so you would set a weight of .1 if you want to normalize them
@@ -54,3 +55,10 @@ for x in range(1,size_x-1):
       if(output_pixel>255):
         output_pixel=255
       i_transformed[x, y] = output_pixel
+
+ # Plot the image. Note the size of the axes -- they are 512 by 512
+plt.gray()
+plt.grid(False)
+plt.imshow(i_transformed)
+#plt.axis('off')
+plt.show()   
