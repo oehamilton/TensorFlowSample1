73e2ed7bae314b47668dab3e0ec22ceefbf19e27
oehamilton <oehamilton@hotmail.com>
2025-06-10 13:10:30 -0500

    Save the size of the image

--- src/CNN.py
+++ src/CNN.py
@@ -11,4 +11,13 @@ plt.axis('off')
 # Display the image using matplotlib
 plt.imshow(i)
 plt.show()
-# Save the image using OpenCV   
+
+
+#Save the size of the image 
+
+i_transformed = np.copy(i)
+size_x = i_transformed.shape[0]
+size_y = i_transformed.shape[1]
+
+print("Size of the image: ", size_x, size_y)
+# Resize the image to 28x28 pixels
