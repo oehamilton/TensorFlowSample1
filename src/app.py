5d2c0c0b97dd65f799119296bb952c7ee7ea1d70
oehamilton <oehamilton@hotmail.com>
2025-06-19 12:59:02 -0500

    Explited conversion to numpy array

--- src/app.py
+++ src/app.py
@@ -1,16 +1,19 @@
 import tensorflow as tf
 import numpy as np
 
+# Define the model
 model = tf.keras.models.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
+# Training data
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0], dtype=float)
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0], dtype=float)
 model.fit(xs, ys, epochs=500, verbose=0)
 
-print(model.predict([10.0]))
-# Output: [[18.999998]]
-print(model.predict([20.0]))
-# Output: [[38.999996]]
-print(model.predict([0.0]))
-
+# Predict with NumPy array input
+input_data = np.array([10.0])
+print(model.predict(input_data))
+# Output: ~[[18.999998]]
+print(model.predict(np.array([20.0])))
+# Output: ~[[38.999996]]
+print(model.predict(np.array([0.0])))
